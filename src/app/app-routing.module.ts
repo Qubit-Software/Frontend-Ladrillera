0aecc8b1148ab51d09afbdc1d536d621179b1884
Juan Diego Tovaria <juken.555@hotmail.com>
2020-07-08 00:16:08 -0500

    Creación de paginas login y home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './Pages/home/home.component';
+import { LoginComponent } from './Pages/login/login.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
-  { path: '', component: HomeComponent}
+  { path: 'login', component: LoginComponent},
+  { path: '', component: LoginComponent}
 ];
 
 @NgModule({
