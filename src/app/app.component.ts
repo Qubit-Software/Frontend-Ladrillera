d019ac588f6d02658fbc393a3a489a7a8a92bab9
Juan Diego Tovaria <juken.555@hotmail.com>
2020-07-11 10:59:59 -0500

    Correccion menu recursivo y enrutamiento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(){}
+
+  ngOnInit(): void {
+    var size;
+    var elemento = document.getElementsByClassName("dropdown-submenu");
+
+    size = window.innerWidth;
+    if (size <= 990) {
+      for (let i = 0; i < elemento.length; i++) {
+        elemento[i].className = "dropdown-submenu dropright"
+      }
+    } else {
+      for (let i = 0; i < elemento.length; i++) {
+        elemento[i].className = "dropdown-submenu dropleft"
+      }
+    }
+    
+    function reportWindowSize() {
+      size = window.innerWidth;
+      console.log(window.innerWidth);
+      if (size <= 990) {
+        for (let i = 0; i < elemento.length; i++) {
+          elemento[i].className = "dropdown-submenu dropright"
+        }
+      } else {
+        for (let i = 0; i < elemento.length; i++) {
+          elemento[i].className = "dropdown-submenu dropleft"
+        }
+      }
+    }
+
+    window.onresize = reportWindowSize;
+  }
   title = 'Ladrillera21';
 }
