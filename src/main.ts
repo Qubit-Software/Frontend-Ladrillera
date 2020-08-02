cf10c4ab9a366f0fe304f64511d4990d4a934ad6
Juan Diego Tovaria <juken.555@hotmail.com>
2020-08-02 16:10:01 -0500

    Cambios previos

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+let $:any;
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  $('[data-toggle="tooltip"]').tooltip({
+    sanitizeFn: function (content) {return content;}
+});
