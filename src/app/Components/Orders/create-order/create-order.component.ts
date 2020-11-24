d3edcf12f8f64229a43867fbd4385e50d024acb4
Juan Diego Tovaria <juken.555@hotmail.com>
2020-11-24 10:48:34 -0500

    Cambios de creación de order logicos finalizados, por generar peticion http

--- src/app/Components/Orders/create-order/create-order.component.ts
+++ src/app/Components/Orders/create-order/create-order.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { product } from "../../../models/products.model";
 @Component({
   selector: 'app-create-order',
   templateUrl: './create-order.component.html',
@@ -8,17 +8,20 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CreateOrderComponent implements OnInit {
 
-  form: FormGroup;
   public totalSell = 0;
+  public product = new product();
+  public dataArray = [];
+  public measure = []
+
   public products = [
     {
       "codigo": "LAD21-MATCO",
-      "nombre": "Bloquelon MATCO",
+      "nombre": ["Bloquelon MATCO"],
       "medida": ["Unidad"]
     },
     {
       "codigo": "LAD21-MALLA",
-      "nombre": "Malla Electrosoldada",
+      "nombre": ["Malla Electrosoldada"],
       "medida": [
         "Rollo/s",
         "Panel/es"
@@ -26,31 +29,62 @@ export class CreateOrderComponent implements OnInit {
     },
     {
       "codigo": "LAD21-PERFIL",
-      "nombre": "Perfil Entrepiso",
+      "nombre": ["Perfil Entrepiso"],
       "medida": ["Metro/s"]
     }
   ]
 
   constructor(private fb: FormBuilder) {
-    this.createForm();
+
   }
 
   ngOnInit(): void {
+    this.product = new product();
+    this.dataArray.push(this.product);
   }
 
-  public onsum(value) {
-    console.log(value);
-    this.totalSell - Number(value);
-    this.totalSell += Number(value);
-  }
 
   public generateOrder() {
-
+    console.log(this.dataArray);
   }
 
-  createForm() {
-    this.form = this.fb.group({
-    });
+
+  public addForm() {
+    this.product = new product();
+    this.dataArray.push(this.product);
+  }
+  public deleteProduct(element) {
+    this.dataArray.splice(element);
   }
 
+  public productCodeSet(element) {
+    switch (this.dataArray[element].nombre) {
+      case "Bloquelon MATCO":
+        this.dataArray[element].codigo_producto = this.products[0].codigo;
+        this.dataArray[element].medidas = this.products[0].medida;
+        break;
+      case "Malla Electrosoldada":
+        this.dataArray[element].codigo_producto = this.products[1].codigo;
+        this.dataArray[element].medidas = this.products[1].medida;
+        break;
+      case "Perfil Entrepiso":
+        this.dataArray[element].codigo_producto = this.products[2].codigo;
+        this.dataArray[element].medidas = this.products[2].medida;
+        break;
+      default:
+        break;
+    }
+  }
+
+  public onChange() {
+    var test = 0;
+    this.totalSell = 0;
+    for (let i = 0; i < this.dataArray.length; i++) {
+      test += this.dataArray[i].valor;
+    }
+    this.totalSell += test;
+    if (isNaN(this.totalSell)) {
+      this.totalSell = 0;
+    }
+  }
 }
---
+++ src/app/models/products.model.ts
@@ -0,0 +1,8 @@
+export class product{
+    codigo_producto:any;
+    nombre:any;
+    cantidad:any;
+    unidad_medicion:any;
+    valor:any;
+    medidas:any[];
+}
