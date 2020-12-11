678f759680bd8b7e9ef36b51c01e2d831b8b7d6a
Juan Diego Tovaria <juken.555@hotmail.com>
2020-12-11 00:01:51 -0500

    componente allemployees creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,6 +39,7 @@ import { LodgeComponent } from './Pages/lodge/lodge.component';
 import { BillingComponent } from './Pages/billing/billing.component';
 import { SearchEmployeeComponent } from './Components/Employee/search-employee/search-employee.component';
 import { ChargeEmployeeComponent } from './Components/Employee/charge-employee/charge-employee.component';
+import { AllEmployeesComponent } from './Components/Employee/all-employees/all-employees.component';
 
 
 @NgModule({
@@ -68,7 +69,8 @@ import { ChargeEmployeeComponent } from './Components/Employee/charge-employee/c
     LodgeComponent,
     BillingComponent,
     SearchEmployeeComponent,
-    ChargeEmployeeComponent
+    ChargeEmployeeComponent,
+    AllEmployeesComponent
   ],
   imports: [
     BrowserModule,
