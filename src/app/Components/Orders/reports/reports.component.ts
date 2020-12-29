a151520a3beefd731f24669a1d7be1828cde1d6d
Juan Diego Tovaria <juken.555@hotmail.com>
2020-12-28 20:06:38 -0500

    Cambios en filtros de reportes  pedidos finalizados

--- src/app/Components/Orders/reports/reports.component.ts
+++ src/app/Components/Orders/reports/reports.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CreateOrderService } from 'src/app/Services/Orders/createOrder/create-order.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-reports',
@@ -7,25 +8,43 @@ import { CreateOrderService } from 'src/app/Services/Orders/createOrder/create-o
   styleUrls: ['./reports.component.css']
 })
 export class ReportsComponent implements OnInit {
-
+  public isChecked: boolean;
   public pedidos: PedidoModel[];
   constructor(private CreateOrderService: CreateOrderService) { }
 
   ngOnInit(): void {
-    this.pedidos = new Array();
-    this.CreateOrderService.getPedidos().subscribe((result: any[]) => {
-      result.forEach(p => {
-        const ped = new PedidoModel();
-        ped.id =  `LAD21-${p.id}`;
-        ped.status = p.estatus;
-        ped.total = p.total;
-        if (ped !== null) {
-          this.pedidos.push(ped);
-        }
-      });
-    });
+    this.getElements(false);
+  }
+  test() {
+    this.getElements(this.isChecked);
   }
 
+  private getElements(finalizados) {
+    
+    if (finalizados == true) {
+      this.pedidos=this.pedidos.filter(element => element.status != "Pedido finalizado");
+    } else {
+      Swal.fire({
+        allowOutsideClick: false,
+        icon: 'info',
+        text: 'Espere por favor'
+      });
+      Swal.showLoading();
+      this.pedidos = new Array();
+      this.CreateOrderService.getPedidos().subscribe((result: any[]) => {
+        result.forEach(p => {
+          const ped = new PedidoModel();
+          ped.id = `LAD21-${p.id}`;
+          ped.status = p.estatus;
+          ped.total = p.total;
+          if (ped !== null) {
+            this.pedidos.push(ped);
+          }
+          Swal.close();
+        });
+      });
+    }
+  }
 }
 export class PedidoModel {
   id: string;
